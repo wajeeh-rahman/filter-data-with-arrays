d70ffd5c08804814e199883c5d3fc0703c529907
Wajih <wajih.09@hotmail.com>
2023-09-06 22:32:13 +1000

    data file and execise file added

---
+++ exercise.js
@@ -0,0 +1,12 @@
+// Task 1 -  extract all data that has the `userId = 1 and userId = 4`
+// console.log(filteredTodos)
+
+// Task 2 - further extract the data (from filteredTodos) that has the completed value `true`
+// console.log(completedTodos)
+
+// Task 3 - Extend task 2 and extract the data (from completedTodos) with the `title` in Ascending order.
+// console.log(sortedTodos)
+
+// Task 4 - console.log total number of sortedTodos
+
+console.log('Hello world!')
